b54afd7c46411d3c5e5ba7ba3db088ceeb8aa035
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-07-18 11:46:46 +0200

    Funcion getById en games.controller y games.model creada y y ruta creada

--- src/app/routes/api/games.api.js
+++ src/app/routes/api/games.api.js
@@ -4,7 +4,7 @@ const gamesController = require('../../controller/games.controller.js');
 const gRouter = Router(); 
 
 gRouter.get('/', gamesController.getAll);
-
+gRouter.get('/:gameId', gamesController.getGame);
 gRouter.post('/insert', gamesController.insertOneGame);
 
 module.exports = gRouter;
