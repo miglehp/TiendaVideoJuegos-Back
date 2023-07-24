209ebb2134b4898dc89aa584e560844fe1897a91
miglehp <migle.hp@gmail.com>
2023-07-24 10:04:34 +0200

    correcciones a la bdd

--- src/temp/crear-bdd.sql
+++ src/temp/crear-bdd.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema ecomercedb
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `ecomercedb` ;
@@ -39,12 +42,27 @@ CREATE TABLE IF NOT EXISTS `ecomercedb`.`games` (
   `publishers` MEDIUMTEXT NULL DEFAULT NULL,
   `price` FLOAT NULL DEFAULT NULL,
   `release_date` VARCHAR(30) NULL DEFAULT NULL,
-  `support_info_url` VARCHAR(200) NULL DEFAULT NULL,
-  `suport_info_email` VARCHAR(100) NULL DEFAULT NULL,
+  `support_info_url` VARCHAR(255) NULL DEFAULT NULL,
+  `suport_info_email` VARCHAR(200) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `steam_appid_UNIQUE` (`steam_app_id` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 605
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `ecomercedb`.`games_has_genres`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ecomercedb`.`games_has_genres` ;
+
+CREATE TABLE IF NOT EXISTS `ecomercedb`.`games_has_genres` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `genres_id` INT NOT NULL,
+  `games_id` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -59,30 +77,36 @@ CREATE TABLE IF NOT EXISTS `ecomercedb`.`genres` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `description_UNIQUE` (`description` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 1769
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `ecomercedb`.`games_has_genres`
+-- Table `ecomercedb`.`pedidos`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ecomercedb`.`games_has_genres` ;
+DROP TABLE IF EXISTS `ecomercedb`.`pedidos` ;
 
-CREATE TABLE IF NOT EXISTS `ecomercedb`.`games_has_genres` (
+CREATE TABLE IF NOT EXISTS `ecomercedb`.`pedidos` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `genres_id` INT NOT NULL,
-  `games_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_genres_has_games_games1_idx` (`games_id` ASC) VISIBLE,
-  INDEX `fk_genres_has_games_genres1_idx` (`genres_id` ASC) VISIBLE,
-  CONSTRAINT `fk_genres_has_games_games1`
-    FOREIGN KEY (`games_id`)
-    REFERENCES `ecomercedb`.`games` (`id`),
-  CONSTRAINT `fk_genres_has_games_genres1`
-    FOREIGN KEY (`genres_id`)
-    REFERENCES `ecomercedb`.`genres` (`id`))
+  `users_id` INT NOT NULL,
+  `estado` ENUM('pendiente', 'preparacion', 'enviado') NOT NULL DEFAULT 'pendiente',
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 1769
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `ecomercedb`.`pedidos_has_games`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ecomercedb`.`pedidos_has_games` ;
+
+CREATE TABLE IF NOT EXISTS `ecomercedb`.`pedidos_has_games` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `pedidos_id` INT NOT NULL,
+  `games_id` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -96,13 +120,9 @@ CREATE TABLE IF NOT EXISTS `ecomercedb`.`screenshots` (
   `games_id` INT NOT NULL,
   `path_thumbnail` VARCHAR(200) NULL DEFAULT NULL,
   `path_full` VARCHAR(200) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_screenshots_games_idx` (`games_id` ASC) VISIBLE,
-  CONSTRAINT `fk_screenshots_games`
-    FOREIGN KEY (`games_id`)
-    REFERENCES `ecomercedb`.`games` (`id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 5931
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -126,49 +146,6 @@ CREATE TABLE IF NOT EXISTS `ecomercedb`.`users` (
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `ecomercedb`.`pedidos`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `ecomercedb`.`pedidos` ;
-
-CREATE TABLE IF NOT EXISTS `ecomercedb`.`pedidos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `users_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_pedidos_users1_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_pedidos_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `ecomercedb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `ecomercedb`.`pedidos_has_games`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `ecomercedb`.`pedidos_has_games` ;
-
-CREATE TABLE IF NOT EXISTS `ecomercedb`.`pedidos_has_games` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `pedidos_id` INT NOT NULL,
-  `games_id` INT NOT NULL,
-  INDEX `fk_pedidos_has_games_games1_idx` (`games_id` ASC) VISIBLE,
-  INDEX `fk_pedidos_has_games_pedidos1_idx` (`pedidos_id` ASC) VISIBLE,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_pedidos_has_games_pedidos1`
-    FOREIGN KEY (`pedidos_id`)
-    REFERENCES `ecomercedb`.`pedidos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pedidos_has_games_games1`
-    FOREIGN KEY (`games_id`)
-    REFERENCES `ecomercedb`.`games` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
