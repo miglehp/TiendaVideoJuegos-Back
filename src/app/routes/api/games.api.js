6dae3690e62783293c3e21641606170743988961
miglehp <migle.hp@gmail.com>
2023-07-24 11:46:56 +0200

    gestión de filtrar por genero en el back + paginación para juegos filtrados por genero

--- src/app/routes/api/games.api.js
+++ src/app/routes/api/games.api.js
@@ -4,14 +4,18 @@ const gamesController = require('../../controller/games.controller');
 const gRouter = Router();
 
 gRouter.get('/', gamesController.getAll);
+gRouter.get('/paginate/:numberPage', gamesController.getGamesByPage);
+
 gRouter.get('/maxPrice', gamesController.getByMaxPrice);
 gRouter.get('/minPrice', gamesController.getByMinPrice);
-gRouter.get('/paginate/:numberPage', gamesController.getGamesByPage);
-gRouter.get('/paginate/:numberPage/name/:gameName', gamesController.getNamesByPage);
-gRouter.get('/paginate/:numberPage/category', gamesController.getCategoryByPage);
-gRouter.get('/paginate/:numberPage/category/:gameCategory', gamesController.filterCategoryByPage);
+
+gRouter.get('/genre/:genreDescription', gamesController.getAllGamesByGenre);
+gRouter.get('/genre/:genreDescription/paginate/:numberPage', gamesController.getGamesByGenreAndPage);
+
 gRouter.get('/:gameId', gamesController.getGame);
+
 gRouter.post('/insert', gamesController.insertOneGame);
+
 gRouter.delete('/:gameId', gamesController.deleteById);
 
 module.exports = gRouter;
