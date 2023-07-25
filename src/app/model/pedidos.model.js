e563cdaad3706d0ed2d467cf495e16c1d9f48fc4
miglehp <migle.hp@gmail.com>
2023-07-25 09:46:02 +0200

    Creados endpoints para gestionar pedidos especificos de un usuario con el check token

--- src/app/model/pedidos.model.js
+++ src/app/model/pedidos.model.js
@@ -2,4 +2,10 @@ const db = require('../../config/db');
 
 const get = () => db.query('SELECT * FROM ecomercedb.pedidos;');
 
-module.exports = { get };
+const getFromUserId = (id) => db.query('SELECT * FROM pedidos WHERE users_id = ?;', [id]);
+
+const getById = (id) => db.query('SELECT * FROM pedidos WHERE id = ?;', [id]);
+
+const newPedido = () => db.query();
+
+module.exports = { get, getFromUserId, getById };
