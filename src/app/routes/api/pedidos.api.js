b6f03373128941d86dfb3b266e0d2f03ff4304ba
miglehp <migle.hp@gmail.com>
2023-07-26 10:11:41 +0200

    corrección en api de pedidos

--- src/app/routes/api/pedidos.api.js
+++ src/app/routes/api/pedidos.api.js
@@ -5,7 +5,7 @@ const pRouter = Router();
 
 // requieren ser admin
 pRouter.get('/', pedidosController.getAll);
-pRouter.get('/:pedidoId', pedidosController.getById);
+pRouter.get('/id/:pedidoId', pedidosController.getById);
 pRouter.put('/:pedidoId', pedidosController.updateState);
 
 // solo requieren estar logado
